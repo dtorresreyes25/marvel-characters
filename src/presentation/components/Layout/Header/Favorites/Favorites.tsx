e4c446b0a4a923368d14039bd424e9b03ba26211
Dayron R. <dayronrafael83@icloud.com>
2025-03-09 16:09:35 +0100

    feat(Header): display favorite count

--- src/presentation/components/Layout/Header/Favorites/Favorites.tsx
+++ src/presentation/components/Layout/Header/Favorites/Favorites.tsx
@@ -2,12 +2,15 @@ import { FC } from 'react';
 import { Container } from './Favorites.style.ts';
 import { ReactComponent as HeartFilledIcon } from '@/assets/heart_filled.svg';
 import Typography from '@/presentation/components/Typography';
+import { useCharacterFavoritesStore } from '@/application/store/useCharacterFavoritesStore';
 
 const Favorites: FC = () => {
+  const { favorites } = useCharacterFavoritesStore();
+
   return (
     <Container>
       <HeartFilledIcon />
-      <Typography variant="md">3</Typography>
+      <Typography variant="md">{favorites?.size}</Typography>
     </Container>
   );
 };
